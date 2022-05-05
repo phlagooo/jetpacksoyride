0861721f63b673b223b27b05867d7a272d0334f7
MelvinAma <melvinamandusson@gmail.com>
2022-05-05 18:52:45 +0200

    Add more user-friendly startup on Mac and add endless scrolling background

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -10,70 +10,81 @@ import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.math.Rectangle;
 
+import static com.badlogic.gdx.graphics.Texture.TextureWrap.Repeat;
+
 public class MyGdxGame extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private OrthographicCamera camera;
-	private Texture steveImage;
-	private Texture stoneImage;
-	private Rectangle steve;
-	private Circle stone;
-	private int steveAcc = -98;
-	private int steveSpeed;
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private Texture steveImage;
+    private Texture stoneImage;
+    private Texture bgImage;
+    private Rectangle steve;
+    private Circle stone;
+    private int steveAcc = -80;
+    private int steveSpeed;
+    private final int WIDTH = 1280;
+    private final int HEIGHT = 720;
+    final int BACKGROUND_SPEED = 6; //
+    final int FLOOR_Y = 100;
+    float sourceX = 0; // Keep track of background
 
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        steveImage = new Texture("badlogic.jpg");
+        stoneImage = new Texture("rock.png");
+        bgImage = new Texture("background.jpg");
+        bgImage.setWrap(Repeat, Repeat);
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, WIDTH, HEIGHT);
 
-		steveImage = new Texture("character.png");
-		stoneImage = new Texture("rock.png");
+        steve = new Rectangle();
+        steve.x = 100;
+        steve.y = 0;
+        steve.height = 50;
+        steve.width = 30;
+    }
 
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 1280, 720);
+    @Override
+    public void render() {
+        ScreenUtils.clear(0, 0, 0, 1);
+        sourceX += BACKGROUND_SPEED;
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
 
-		steve = new Rectangle();
-		steve.x = 1280 / 6;
-		steve.y = 0;
-		steve.height = 100;
-		steve.width = 80;
+        batch.begin();
+        sourceX %= bgImage.getWidth();
+        // Moving background
+        batch.draw(bgImage,
+                // position and size of texture
+                0, 0, WIDTH, HEIGHT,
+                // srcX, srcY, srcWidth, srcHeight
+                (int) sourceX, 0, bgImage.getWidth(), bgImage.getHeight(),
+                // flipX, flipY
+                false, false);
+        batch.draw(steveImage, steve.x, steve.y);
+        batch.end();
 
+        // Jump
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            steveSpeed = 1200;
+        }
 
-	}
+        steve.y += steveSpeed * Gdx.graphics.getDeltaTime();
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(0, 0, 0, 1);
-		camera.update();
+        if (steve.y > FLOOR_Y) {
+            steveSpeed += steveAcc;
+        } else if (steve.y == FLOOR_Y) {
+            steveSpeed = 0;
+        } else if (steve.y < FLOOR_Y) {
+            steveSpeed = 0;
+            steve.y = FLOOR_Y;
+        }
+    }
 
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		batch.draw(steveImage, steve.x, steve.y);
-		batch.end();
-
-
-		if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE)){
-			steveSpeed = 1000;
-		}
-
-		steve.y += steveSpeed * Gdx.graphics.getDeltaTime();
-
-		if(steve.y > 0){
-			steveSpeed += steveAcc;
-		}else if(steve.y == 0){
-			steveSpeed = 0;
-		}else if(steve.y < 0){
-			steveSpeed = 0;
-			steve.y = 0;
-		}
-
-
-
-
-
-	}
-
-	@Override
-	public void dispose () {
-		batch.dispose();
-	}
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
 }
