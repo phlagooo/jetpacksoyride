4b41b65cf6dbfc92749db141cede631f64c87738
MelvinAma <63121251+MelvinAma@users.noreply.github.com>
2022-05-19 00:40:36 +0200

    Add run/jump animations and new game theme

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -8,8 +8,10 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -23,12 +25,11 @@ import static com.mygdx.game.Rock.*;
 public class MyGdxGame extends ApplicationAdapter {
     private SpriteBatch batch;
     private OrthographicCamera camera;
-    private Texture steveImage;
     private Texture bgImage;
     private Rectangle steve;
     private Circle stone;
     private int steveAcc = -80;
-    private int steveSpeed;
+    private int steve_y_speed;
     private final int WIDTH = 1280;
     private final int HEIGHT = 720;
     private int backgroundSpeed = 6; //
@@ -40,11 +41,17 @@ public class MyGdxGame extends ApplicationAdapter {
     private Music mainMusic;
     private Sound deathSound;
     private Sound jumpSound;
+    float elapsedTime;
     private int xStartPos = 100;
     private int yStartPos = FLOOR_Y;
     private int deathCount;
-    private int survive;
+    private int survivedFrames;
     private BitmapFont font;
+    // Movement textures / animations
+    private Texture jump;
+    Animation<TextureRegion> currentSteveAnimationState;
+    Animation<TextureRegion> runAnimation;
+    Animation<TextureRegion> fallAnimation;
 
 
     @Override
@@ -52,48 +59,49 @@ public class MyGdxGame extends ApplicationAdapter {
         rocks = new Array<>();
         // Create all rocks to be used
         for (int i = 0; i < ROCK_COUNT; i++) {
-            rocks.add(new Rock((i+1) * (rand.nextInt(FLUCTUATION) + MINIMUM_GAP) + WIDTH));
+            rocks.add(new Rock((i + 1) * (rand.nextInt(FLUCTUATION) + MINIMUM_GAP) + WIDTH));
         }
         batch = new SpriteBatch();
-        steveImage = new Texture("belle.png");
         bgImage = new Texture("background.jpg");
         bgImage.setWrap(Repeat, Repeat);
         camera = new OrthographicCamera();
         camera.setToOrtho(false, WIDTH, HEIGHT);
         font = new BitmapFont();
         font.setColor(Color.BLACK);
-        mainMusic = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+        mainMusic = Gdx.audio.newMusic(Gdx.files.internal("joyrideTheme.mp3"));
         deathSound = Gdx.audio.newSound(Gdx.files.internal("death.mp3"));
         jumpSound = Gdx.audio.newSound(Gdx.files.internal("jump (on chicken).mp3"));
+        // Reference: https://gamedev.stackexchange.com/questions/136659/is-it-possible-to-use-animated-gif-images-in-lbgdx
+        runAnimation = GifDecoder.loadGIFAnimation(Animation.PlayMode.LOOP, Gdx.files.internal("run.gif").read());
+        jump = new Texture("jump.png");
         mainMusic.setLooping(true);
+        mainMusic.setVolume(0.6f);
         mainMusic.play();
 
         steve = new Rectangle();
         steve.x = xStartPos;
         steve.y = yStartPos;
-        steve.width = 150;
-        steve.height = 180;
+        steve.width = 140;
+        steve.height = 140;
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0, 0, 0, 1);
+        elapsedTime += Gdx.graphics.getDeltaTime();
         sourceX += backgroundSpeed;
         camera.update();
         batch.setProjectionMatrix(camera.combined);
 
-
-
         // Prevent player from clipping the top of the window
         if (steve.y + steve.height >= HEIGHT) {
             steve.y = HEIGHT - steve.height;
-            steveSpeed += steveAcc;
+            steve_y_speed += steveAcc;
         }
 
         // Start render
         batch.begin();
         sourceX %= bgImage.getWidth();
-
 
         // Draw background
         batch.draw(bgImage,
@@ -107,40 +115,47 @@ public class MyGdxGame extends ApplicationAdapter {
         // Handle and draw obstacles
         collisionLogic();
 
-        batch.draw(steveImage, steve.x, steve.y, steve.width, steve.height);
-        font.draw(batch, Integer.toString(survive/60), WIDTH - 30, HEIGHT - 20);
+        if (steve.y > FLOOR_Y) {
+            // Steve is in the air
+            batch.draw(jump, steve.x, steve.y, steve.width, steve.height);
+        } else {
+            batch.draw(runAnimation.getKeyFrame(elapsedTime), steve.x, steve.y, steve.width, steve.height);
+        }
+        font.draw(batch, Integer.toString(survivedFrames / 60), WIDTH - 30, HEIGHT - 20);
         batch.end(); // Frame finished
 
         // Jump
         if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
-            jumpSound.play();
-            steveSpeed = 1200;
+            jumpSound.play(0.5f);
+            steve_y_speed = 1200;
         }
 
         // Handle gravity
-        steve.y += steveSpeed * Gdx.graphics.getDeltaTime();
-        if (steve.y > FLOOR_Y) {
-            steveSpeed += steveAcc;
+        steve.y += steve_y_speed * Gdx.graphics.getDeltaTime();
+        if (steve.y > FLOOR_Y) { // Steve is in the air
+            steve_y_speed += steveAcc;
+            currentSteveAnimationState = fallAnimation;
         } else if (steve.y == FLOOR_Y) {
-            steveSpeed = 0;
-        } else if (steve.y < FLOOR_Y) {
-            steveSpeed = 0;
+            steve_y_speed = 0;
+            currentSteveAnimationState = runAnimation;
+        } else if (steve.y < FLOOR_Y) { // Steve is below the floor
+            steve_y_speed = 0;
             steve.y = FLOOR_Y;
         }
     }
 
     private void collisionLogic() {
-      survive++;
-      for (int i = 0; i < rocks.size; i++) {
+        survivedFrames++;
+        for (int i = 0; i < rocks.size; i++) {
             // Check for collision
             if (steve.overlaps(rocks.get(i).bounds)) {
                 deathSound.play();
                 deathCount++;
                 sourceX = 0;
-                survive = 0;
+                survivedFrames = 0;
                 // Randomly arrange all crates to the right of the screen
                 for (int j = 0; j < rocks.size; j++) {
-                    rocks.get(j).reposition((j+1) * (rand.nextInt(FLUCTUATION) + MINIMUM_GAP) + WIDTH);
+                    rocks.get(j).reposition((j + 1) * (rand.nextInt(FLUCTUATION) + MINIMUM_GAP) + WIDTH);
                 }
                 break;
             }
--- core/src/com/mygdx/game/Rock.java
+++ core/src/com/mygdx/game/Rock.java
@@ -1,6 +1,8 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
